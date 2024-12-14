2075fe9eefa1aca7c60a7d4d7dac0ec11db0ff3f
Ivan-parcina <ivan.parcina1@gmail.com>
2024-12-14 22:30:04 +0100

    feat: style header and navigation, add logout button

--- IThive/src/main/java/com/itm/ithive/controller/HomeController.java
+++ IThive/src/main/java/com/itm/ithive/controller/HomeController.java
@@ -7,21 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-import java.util.List;
-
 //@RestController
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping
-    public String home(Model model, Principal principal) {
-        //Principal - get data from current authorized user
-        if (principal != null) {
-            String username = principal.getName();
-            model.addAttribute("username", username);
-        }
-        return "test";
+    public String home(Model model) {
+        model.addAttribute("Home", "Welcome to IThive");
+        return "home";
     }
 }
